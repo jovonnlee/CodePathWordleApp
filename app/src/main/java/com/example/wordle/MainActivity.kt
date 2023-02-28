c22e0fdec6b1645b0146297a3c529ae511a01dfa
jovonnlee <jovonnlee@gmail.com>
2023-02-28 00:39:59 -0800

    2nd

--- app/src/main/java/com/example/wordle/MainActivity.kt
+++ app/src/main/java/com/example/wordle/MainActivity.kt
@@ -67,17 +67,16 @@ class MainActivity : AppCompatActivity() {
         g1.visibility = View.INVISIBLE
         g2.visibility = View.INVISIBLE
         g3.visibility = View.INVISIBLE
-        g4.visibility = View.INVISIBLE
+
 
         g1c.visibility = View.INVISIBLE
         g2c.visibility = View.INVISIBLE
         g3c.visibility = View.INVISIBLE
-        g4c.visibility = View.INVISIBLE
 
         word1.visibility = View.INVISIBLE
         word2.visibility = View.INVISIBLE
         word3.visibility = View.INVISIBLE
-        word4.visibility = View.INVISIBLE
+
 
         button.text = btnGuess
         isCorrect = false
@@ -92,15 +91,15 @@ class MainActivity : AppCompatActivity() {
         val g1 = findViewById<TextView>(R.id.G1)
         val g2 = findViewById<TextView>(R.id.G2)
         val g3 = findViewById<TextView>(R.id.G3)
-        val g4 = findViewById<TextView>(R.id.G4)
+
         val g1c = findViewById<TextView>(R.id.G1C)
         val g2c = findViewById<TextView>(R.id.G2C)
         val g3c = findViewById<TextView>(R.id.G3C)
-        val g4c = findViewById<TextView>(R.id.G4C)
+
         val word1 = findViewById<TextView>(R.id.word1)
         val word2 = findViewById<TextView>(R.id.word2)
         val word3 = findViewById<TextView>(R.id.word3)
-        val word4 = findViewById<TextView>(R.id.word4)
+
         val win = findViewById<TextView>(R.id.WIN)
         val answer = findViewById<TextView>(R.id.answer)
         val button = findViewById<Button>(R.id.button)
@@ -132,26 +131,20 @@ class MainActivity : AppCompatActivity() {
                     word3.text = input.text.toString().uppercase()
                     word3.visibility = View.VISIBLE
                 }
-                4 -> {
-                    g4c.visibility = View.VISIBLE
-                    g4c.text = checkGuess(input.text.toString().uppercase())
-                    g4.visibility = View.VISIBLE
-                    word4.text = input.text.toString().uppercase()
-                    word4.visibility = View.VISIBLE
-                }
+
             }
-            if (counter == 4) {
+            if (counter == 3) {
                 answer.visibility = View.VISIBLE
                 button.text = btnReset
             }
-            if (counter == 5) {
+            if (counter == 4) {
                 reset()
             }
             if (isCorrect){
                 answer.visibility = View.VISIBLE
                 win.visibility = View.VISIBLE
                 button.text = btnReset
-                counter = 4
+                counter = 3
             }
 
             input.setText("")
